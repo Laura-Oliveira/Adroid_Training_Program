b81d173c982052e47c460c26a5e2c7836c53ebbd
Laura-Oliveira <laura.regina.cp@gmail.com>
2020-10-21 14:45:27 -0300

    Initial commit

---
+++ app/src/main/java/com/adroid_training_program/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adroid_training_program
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
