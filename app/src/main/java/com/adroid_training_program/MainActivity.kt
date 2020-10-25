f1798285d90c6034ceafdba7f29ba7c1f1e452a7
Laura-Oliveira <laura.regina.cp@gmail.com>
2020-10-25 12:01:28 -0300

    Implementação da Tela Inicial

--- app/src/main/java/com/adroid_training_program/MainActivity.kt
+++ app/src/main/java/com/adroid_training_program/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.adroid_training_program
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun nextPageMenu(view: View)
+    {
+        val intent = Intent(this, MenuActivity::class.java);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/adroid_training_program/MenuActivity.kt
@@ -0,0 +1,13 @@
+package com.adroid_training_program
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
